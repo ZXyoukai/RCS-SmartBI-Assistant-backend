3c131f0d4684eb8d3515fc4f5e7f1f349f83a7e5
ZXyoukai <domingosgermanofrancos@gmail.com>
2025-08-09 16:46:35 +0100

    feat: adding getUser for the api and more changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,25 @@
 const userService = require('../services/userService');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 exports.getAllUsers = async (req, res) => {
   const users = await userService.getAllUsers();
   res.json(users);
 };
+
+exports.getUser = async (req, res ) => {
+
+  const id = req.params.id;
+  if (!id) return res.status(401).json({ error: 'Id obrigatório' });
+  try {
+    const user = await prisma.users.findUnique({ where: { id: Number(id) } });
+    if (!user) return res.status(404).json({ error: 'Usuário não encontrado' });
+    return res.status(200).json(user);
+  } catch (error) {
+    if (!user) return res.status(404).json({ error: 'erro ao buscar Usuario' });
+      res.json(user);
+  }
+}
 
 exports.createUser = async (req, res) => {
   const user = await userService.createUser(req.body);
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const auth = require("../middleware/authMiddleware")
 
-router.get('/', userController.getAllUsers);
+router.get('/', auth ,userController.getAllUsers);
+router.get('/:id', userController.getUser);
+router.get('/allowed', userController.getAllUsers);
 router.post('/', userController.createUser);
 
 module.exports = router;
