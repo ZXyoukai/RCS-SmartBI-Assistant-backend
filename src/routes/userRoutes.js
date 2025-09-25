13f9ce0f87d1799b83b8fd5605e3afbc0825c978
ZXyoukai <martinhocarneiros@gmail.com>
2025-09-25 15:42:24 +0100

    feat:  add routes for users/me

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,6 +6,15 @@ const { authMiddleware } = require("../middleware/authMiddleware")
 router.get('/', authMiddleware, userController.getAllUsers);
 router.get('/:id', userController.getUser);
 router.get('/allowed', userController.getAllUsers);
+router.post('/me', userController.getUserByToken);
 router.post('/', userController.createUser);
 
 module.exports = router;
+
+
+
+
+
+
+
+
