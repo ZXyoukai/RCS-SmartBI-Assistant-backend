c6a109a7c048fc5b9412bd33c58c7850810a2a7d
ZXyoukai <martinhocarneiros@gmail.com>
2025-10-06 10:07:58 +0100

    refactor: enhance 404 handler to include requested path in response

--- src/app.js
+++ src/app.js
@@ -75,11 +75,12 @@ app.use((err, req, res, next) => {
   });
 });
 
-// 404 handler
-app.use('*', (req, res) => {
+// 404 handler - catch all undefined routes
+app.all('*', (req, res) => {
   res.status(404).json({
     success: false,
-    message: 'Route not found'
+    message: 'Route not found',
+    path: req.originalUrl
   });
 });
 
